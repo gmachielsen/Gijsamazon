2ee1da8e2022a68eac25b2b9fd439a9fb1b2ed2b
Gijs Machielsen <g.machielsen@gmail.com>
2020-04-19 16:15:49 +0200

    update product form

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -7,9 +7,9 @@
           <h1 class="a-size-large a-spacing-none a-text-normal">All products</h1>
           <div class="a-spacing-large"></div>
           <!-- Button -->
-          <a href="#" class="a-button-buy-again">Add a new product</a>
-          <a href="#" class="a-button-history margin-right-10">Add a new category</a>
-          <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
+          <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
+          <nuxt-link to="/category" class="a-button-history margin-right-10">Add a new category</nuxt-link>
+          <nuxt-link to="/owner" class="a-button-history margin-right-10">Add a new owner</nuxt-link>
 
           <!-- Listing page -->
         </div>
@@ -52,7 +52,7 @@
             </div>
             <!-- Product Button -->
             <div class="a-row">
-              <a href="#" class="a-button-history margin-right-10">Update</a>
+              <nuxt-link :to="`/products/${product._id}`" class="a-button-history margin-right-10">Update</nuxt-link>
               <a href="#" class="a-button-history margin-right-10">Update</a>
             </div>
           </div>
