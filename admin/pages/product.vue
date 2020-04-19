d8844da5b3855b63edc74f857606789e343663da
Gijs Machielsen <g.machielsen@gmail.com>
2020-04-19 13:06:56 +0200

    initial commit add product page

--- admin/pages/product.vue
+++ admin/pages/product.vue
@@ -11,7 +11,7 @@
                             <!-- Category Dropdown -->
                             <div class="a-spacing-top-medium">
                                 <label>Category</label>
-                                <select class="a-select-option">
+                                <select class="a-select-option" v-model="categoryID">
                                     <option v-for="category in categories" :value="category._id" :key="category._id">{{ category.type }}</option>
                                 </select>
                             </div>
@@ -20,7 +20,7 @@
                             <!-- Owner Dropdown -->
                             <div class="a-spacing-top-medium">
                                 <label>Owner</label>
-                                <select class="a-select-option">
+                                <select class="a-select-option" v-model="ownerID">
                                     <option v-for="owner in owners" :value="owner._id" :key="owner._id">{{ owner.name }}</option>
                                 </select>
                             </div>
@@ -28,19 +28,25 @@
                             <!-- Title input -->
                             <div class="a-spacing-top-medium">
                                 <label>Title</label>
-                                <input type="text" class="a-input-text" style="width: 100%" />
+                                <input type="text" class="a-input-text" style="width: 100%" v-model="title" />
                             </div>
 
                             <!-- Price input -->
                             <div class="a-spacing-top-medium">
+                                <label>Stock Quantity</label>
+                                <input type="number" class="a-input-text" style="width: 100%" v-model="price" />
+                            </div>
+
+                            <!-- stockQuantity input -->
+                            <div class="a-spacing-top-medium">
                                 <label>Price</label>
-                                <input type="number" class="a-input-text" style="width: 100%" />
+                                <input type="number" class="a-input-text" style="width: 100%" v-model="stockQuantity" />
                             </div>
 
                             <!-- Description -->
                             <div class="a-spacing-top-medium">
                                 <label>Description</label>
-                                <textarea placeholder="Provide details such as a product description" style="width: 100%" name="" id="" cols="30" rows="5"></textarea>
+                                <textarea placeholder="Provide details such as a product description" style="width: 100%" name="" id="" cols="30" rows="5" v-model="description"></textarea>
                             </div>
 
                             <!-- Photo  -->
@@ -49,8 +55,8 @@
                                 <div class="a-row a-spacing-top-medium">
                                     <label class="choosefile-button">
                                         <i class="fal fa-plus"></i>
-                                        <input type="file" />
-                                        <p style="width: 100%">name of the photo</p>
+                                        <input type="file" @change="onFileselected" />
+                                        <p style="width: 100%">{{ fileName }}</p>
                                     </label>
                                 </div>
                             </div>
@@ -59,7 +65,7 @@
                             <div class="a-spacing-top-large">
                                 <span class="a-button-register">
                                     <span class="a-button-inner">
-                                        <span class="a-button-text">Add product</span>
+                                        <span class="a-button-text" @click="onAddProduct">Add product</span>
                                     </span>
                                 </span>
                             </div>
@@ -89,6 +95,41 @@ export default {
             };
         } catch (err) {
             console.log(err);
+        }
+    },
+
+    data() {
+        return {
+            categoryID: null,
+            ownerID: null,
+            title: "",
+            price: 0,
+            description: "",
+            selectedFile: null,
+            stockQuantity: 1,
+            fileName: ""
+        }
+    },
+
+    methods: {
+        onFileselected(event) {
+            this.selectedFile = event.target.files[0];
+            this.fileName = event.target.files[0].name;
+        },
+
+        async onAddProduct() {
+            let data = new FormData();
+            data.append("title", this.title);
+            data.append("price", this.price);
+            data.append("description", this.description);
+            data.append("ownerID", this.ownerID);
+            data.append("stockQuantity", this.stockQuantity);
+            data.append("categoryID", this.categoryID);
+            data.append("photo", this.selectedFile, this.selectedFile.name);
+
+            let result = await this.$axios.$post("http://localhost:3000/api/products", data);
+
+            this.$router.push("/")
         }
     }
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -6,6 +6,8 @@ const upload = require("../middlewares/upload-photo");
 router.post('/products', upload.single("photo"), async (req, res) => {
     try {
         let product = new Product();
+        product.ownerID = req.body.ownerID;
+        product.categoryID = req.body.categoryID;
         product.title = req.body.title;
         product.description = req.body.description;
         product.price = req.body.price;
